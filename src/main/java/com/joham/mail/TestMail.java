9a94803752dd6e898b1bfc6ab3d6c0cb8341ab3d
2qiaoyu@163.com <2qiaoyu@163.com>
2016-03-10 09:27:21 +0800

    调整

--- src/main/java/com/joham/mail/TestMail.java
+++ src/main/java/com/joham/mail/TestMail.java
@@ -6,14 +6,14 @@ package com.joham.mail;
 public class TestMail {
     public static void main(String[] args) {
         String smtp = "smtp.exmail.qq.com";
-        String from = "qiaoyu@qianmi.com";
-        String to = "2qiaoyu@163.com";
-        String copyto = "694113006@qq.com";
+        String from = "*****";
+        String to = "*****";
+        String copyto = "*****";
         String subject = "邮件主题";
         String content = "邮件内容";
-        String username = "qiaoyu@qianmi.com";
-        String password = "Joham880419";
-        String filename = "E:\\项目\\奥天\\新问题.doc";
+        String username = "*****";
+        String password = "******";
+        String filename = "E:\\test\\test.txt";
         Mail.sendAndCc(smtp, from, to, copyto, subject, content, username, password, filename);
     }
 }
