511f8459c6d7a49a1c326db90d350f0849fcf2f2
joham <2qiaoyu@163.com>
2016-11-09 20:02:42 +0800

    fastjson修改

--- src/main/java/com/joham/fastjson/TestFastJson.java
+++ src/main/java/com/joham/fastjson/TestFastJson.java
@@ -1,84 +1,67 @@
 package com.joham.fastjson;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 
 import java.util.*;
 
 /**
- * fastjson map,list使用
+ * fastJson使用
  */
 public class TestFastJson {
     public static void main(String[] args) {
-//        test();
-//        test1();
-//        map2JSON();
-//        list2JSON();
-        test2();
+        javaBeanToJson();
+        listToJson();
+        mapToJson();
     }
 
     /**
-     * 类转换为json
+     * JavaBean Json 互转
      */
-    public static void test() {
+    public static void javaBeanToJson() {
+        System.out.println("--------------------JavaBean转换为Json----------------------");
         UserInfo userInfo = new UserInfo();
-        userInfo.setName("joham");
+        userInfo.setName("小明");
         userInfo.setAge(18);
-        System.out.println(JSON.toJSON(userInfo));
-    }
-
-    /**
-     * 解析json成类
-     */
-    public static void test1() {
-        String json = "{\"name\":\"joham\",\"age\":18}";
-        UserInfo userInfo = JSON.parseObject(json, UserInfo.class);
-        System.out.println(userInfo.getName() + "---" + userInfo.getAge());
-    }
-
-    /**
-     * map，json互转
-     */
-    public static void map2JSON() {
-        Map<Object, Object> map = new HashMap<Object, Object>();
-        map.put("a", "aaa");
-        map.put("b", "bbb");
-        map.put("c", "ccc");
-        String json = JSON.toJSONString(map);
+        String json = JSON.toJSONString(userInfo);
         System.out.println(json);
-        Map map1 = JSON.parseObject(json);
-        for (Object o : map1.entrySet()) {
-            Map.Entry<String, String> entry = (Map.Entry<String, String>) o;
-            System.out.println(entry.getKey() + "--->" + entry.getValue());
-        }
+        System.out.println("--------------------Json转换为JavaBean----------------------");
+        UserInfo userInfo1 = JSON.parseObject(json, UserInfo.class);
+        System.out.println(userInfo1.getName() + "---" + userInfo1.getAge());
     }
 
     /**
-     * list，json互转
+     * List<JavaBean> Json互转
      */
-    public static void list2JSON() {
-        List<Object> list = new ArrayList<Object>();
-        list.add("aaa");
-        list.add("bbb");
-        list.add("ccc");
+    public static void listToJson() {
+        List<UserInfo> list = new ArrayList<UserInfo>();
+        list.add(new UserInfo("小红", 20));
+        list.add(new UserInfo("小强", 22));
         String json = JSON.toJSONString(list);
+        System.out.println("--------------------List<JavaBean>转换为Json----------------------");
         System.out.println(json);
-        List<Object> list1 = JSON.parseObject(json, new TypeReference<List<Object>>() {
-        });
-        for (int i = 0; i < list.size(); i++) {
+        List<UserInfo> list1 = JSON.parseArray(json, UserInfo.class);
+        System.out.println("--------------------Json转换为List<JavaBean>----------------------");
+        for (int i = 0; i < list1.size(); i++) {
             System.out.println(list1.get(i));
         }
     }
 
     /**
-     * 日期格式化
+     * Map Json互转
      */
-    public static void test2() {
-        Date date = new Date();
-        String json = JSON.toJSONString(date);
+    public static void mapToJson() {
+        Map<Object, Object> map = new HashMap<Object, Object>();
+        map.put("a", "aaa");
+        map.put("b", "bbb");
+        map.put("c", "ccc");
+        String json = JSON.toJSONString(map);
+        System.out.println("--------------------Map转换为Json----------------------");
         System.out.println(json);
-        System.out.println(JSON.toJSONString(date, SerializerFeature.WriteDateUseDateFormat));
-        System.out.println(JSON.toJSONStringWithDateFormat(date, "yyyy-MM-dd", SerializerFeature.WriteDateUseDateFormat));
+        Map map1 = JSON.parseObject(json);
+        System.out.println("--------------------Json转换为Map----------------------");
+        for (Object o : map1.entrySet()) {
+            Map.Entry<String, String> entry = (Map.Entry<String, String>) o;
+            System.out.println(entry.getKey() + "--->" + entry.getValue());
+        }
     }
 }
