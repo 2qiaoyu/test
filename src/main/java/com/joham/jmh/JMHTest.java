c58537c24223dd2ed13072a5b28e0379f644559a
乔雨 <qiaoyu@qianmi.com>
2017-03-19 13:10:35 +0800

    JVM测试类

---
+++ src/main/java/com/joham/jmh/JMHTest.java
@@ -0,0 +1,28 @@
+package com.joham.jmh;
+
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+/**
+ * JMH JVM性能测试
+ * @author qiaoyu
+ */
+public class JMHTest {
+
+    @GenerateMicroBenchmark
+    public void wellHelloThere() {
+        // this method was intentionally left blank.
+    }
+
+    public static void main(String[] args) throws RunnerException{
+        Options opt = new OptionsBuilder()
+                .include(".*" + JMHTest.class.getSimpleName() + ".*")
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+}
