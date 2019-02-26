2d82d0fd4016a06832f18b594780c6f8a0471150
乔雨 <qiaoyu@qianmi.com>
2019-02-26 15:06:01 +0800

    ArrayList的一些异常

--- src/main/java/com/joham/list/ArrayListTest.java
+++ src/main/java/com/joham/list/ArrayListTest.java
@@ -1,17 +1,26 @@
 package com.joham.list;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
- * subList造成异常
+ * ArrayList的一些异常
  *
  * @author joham
  */
 public class ArrayListTest {
 
     public static void main(String[] args) {
-        List masterList = new ArrayList();
+        testSubList();
+        testArray();
+    }
+
+    /**
+     * 在subList场景中，高度注意对原列表的修改，会导致子列表的遍历、增加、删除均产生ConcurrentModificationException异常。
+     */
+    private static void testSubList() {
+        List<String> masterList = new ArrayList<>();
         masterList.add("one");
         masterList.add("two");
         masterList.add("three");
@@ -20,6 +29,17 @@ public class ArrayListTest {
         //抛出异常
         masterList.add("four");
         for (Object t : branchList) {
+            System.out.println(t);
+        }
+    }
+
+    /**
+     * 使用工具类Arrays.asList()把数组转换成集合时，不能使用其修改集合相关的方法，它的add/remove/clear方法会抛出UnsupportedOperationException异常。
+     */
+    private static void testArray() {
+        List<String> list = Arrays.asList("one", "two", "three");
+        list.add("four");
+        for (Object t : list) {
             System.out.println(t);
         }
     }
